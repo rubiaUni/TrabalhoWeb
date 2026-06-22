c0c7fb1cdea7f003cdf243214d4fc8572a3899bd
willianleoni <willianleoni@gmail.com>
2026-06-22 19:46:48 -0300

    feat: add photo upload (multer) and QR code generation

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import notebookRoutes from './routes/routes.js';
 import categoryRoutes from './routes/category.routes.js';
+import uploadRoutes from './routes/upload.routes.js';
 
 const app = express();
 app.use(express.json());
+app.use('/uploads', express.static('/data/uploads'));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'API is running!' });
@@ -11,5 +13,6 @@ app.get('/', (req, res) => {
 
 app.use('/notebooks', notebookRoutes);
 app.use('/categories', categoryRoutes);
+app.use('/uploads', uploadRoutes);
 
 export default app;
